503364b55a6e25cfda43e58261d767f40d4af85a
StaceyO21 <staceyobrien21@gmail.com>
2024-05-22 22:14:15 -0500

    Diamond

--- functions/try-it/isPalindrome.js
+++ functions/try-it/isPalindrome.js
@@ -1,7 +1,79 @@
-function reverse(str) {
-   return str.split('').reverse().join('');
+function makeLine(size) {
+   let lineMade = '';
+   for(let i = 0; i < size; i++){
+      lineMade += '#';
+      }
+      return lineMade;
+   }
+
+console.log(makeLine(5));
+
+function makeSquare(size) {
+   let square = '';
+   for(let i = 0; i < size; i++){
+      console.log(makeLine(5));
+   }
+   return square;
+}
+console.log(makeSquare(5));
+
+function makeRectangle(width, height) {
+   let rectangle = '';
+   for (let i = 0; i < height; i++) {
+     rectangle += (makeLine(width) + '\n');
+   }
+   return rectangle.slice(0, -1);
+ }
+console.log(makeRectangle(5,3));
+
+function makeDownwardStairs(height) {
+   let stairs = '';
+   for(let i = 0; i < height; i++) {
+      stairs += (makeLine(i+1) + '\n');
+   }
+   return stairs.slice(0,-1);
+}
+console.log(makeDownwardStairs(5));
+
+function makeSpaceLine(numSpaces, numChars){
+   let spaceLine = "";
+   for(let i = 0; i < numSpaces; i++){
+      spaceLine += " ";
+   }
+   for(let i = 0; i < numChars; i++){
+      spaceLine += "#";
+   }
+   for(let i = 0; i < numSpaces; i++){
+      spaceLine += " ";
+   }
+   return spaceLine;
+}
+console.log(makeSpaceLine(3,5));
+
+function makeIsoscelesTriangle(height){
+   let triangle = '';
+   for(let i = 0; i < height; i++) {
+      triangle += (makeSpaceLine(height - i - 1, 2*i + 1) + '\n');
+   }
+   return triangle.slice(0,-1);
+}
+console.log(makeIsoscelesTriangle(5));
+
+function reverseString(str) {
+   let reversedString = "";
+   for(i = str.length-1; i >= 0; i--) {
+       reversedString += str[i];
+   }
+   return reversedString;
+}
+function makeDiamond(height) {
+   let diamond = "";
+   triangle = makeIsoscelesTriangle(height);
+   for(let i = 0; i < height; i++) {
+      triangle += (makeSpaceLine(height - i - 1, 2*i + 1) + '\n');
+   
+   }
+  return diamond;
 }
 
-function isPalindrome(str) {
-   return reverse(str) === str;
-}
+console.log(makeDiamond(5));;
