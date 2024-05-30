2afbc1c60e2f04e3768951cc85e362ec84883b8a
StaceyO21 <staceyobrien21@gmail.com>
2024-05-29 22:26:52 -0500

    Anonymous Function Exercises

--- more-on-functions/exercises/raid-a-shuttle.js
+++ more-on-functions/exercises/raid-a-shuttle.js
@@ -24,34 +24,33 @@ let cargoHold = ['meal kits', 'space suits', 'first-aid kit', 'satellite', 'gold
 console.log("Fuel level: " + checkFuel(fuelLevel));
 console.log("Hold status: " + holdStatus(cargoHold));
 
-/* Steal some fuel from the shuttle:
- * /
- 
-//a). Define an anonymous function and set it equal to a variable with a normal, non-suspicious name. The function takes one parameter. This will be the fuel level on the shuttle.
+let norm = function (a) {
+  if (checkFuel(a) === 'green') {
+    return a - 100001;
+  }
+  else if (checkFuel(a) === 'yellow') {
+    return a - 5001;
+  }
+  else {
+    return a;
+  }
+};
+console.log(norm(fuelLevel));
 
-//b). You must siphon off fuel without alerting the TAs. Inside your function, you want to reduce the fuel level as much as possible WITHOUT changing the color returned by the checkFuel function.
+let innocentVar = function (loot) {
+ let swag = [];
+ swag.push(cargoHold.splice(loot[0],1, 'rock'));
+ swag.push(cargoHold.splice(loot[1],1, 'husband'))
+  return swag;
+}
 
-//c). Once you figure out how much fuel to pump out, return that value.
+// console.log(innocentVar());
+// console.log(cargoHold);
 
-//d). Decide where to best place your function call to gather our new fuel.
-
-/* Next, liberate some of that glorious cargo.
- * /
-
-//a). Define another anonymous function with an array as a parameter, and set it equal to another innocent variable.
-
-//b). You need to swipe two items from the cargo hold. Choose well. Stealing water ain’t gonna get us rich. Put the swag into a new array and return it from the function.
-
-//c). The cargo hold has better security than the fuel tanks. It counts how many things are in storage. You need to replace what you steal with something worthless. The count MUST stay the same, or you’ll get caught and thrown into the LaunchCode brig.
-
-//d). Don’t get hasty, matey! Remember to test your function.
-
-/* Finally, you need to print a receipt for the accountant. Don’t laugh! That genius knows MATH and saves us more gold than you can imagine.
- * /
- 
-//a). Define a function called irs that can take fuelLevel and cargoHold as arguments.
-	
-//b). Call your anonymous fuel and cargo functions from within irs.
-
-//c). Use a template literal to return, "Raided _____ kg of fuel from the tanks, and stole ____ and ____ from the cargo hold."
-
+let irs = function (fuelLevel,stuffToSteal){
+  
+let legallyProcuredGoods = innocentVar(stuffToSteal);
+console.log(`Raided ${norm(fuelLevel)} kg of fuel from the tanks, and stole ${legallyProcuredGoods[0]} and ${legallyProcuredGoods[1]} from the cargo hold.`)
+}
+let wishList = [3,4];
+console.log(irs(fuelLevel,wishList));
