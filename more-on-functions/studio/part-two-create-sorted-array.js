8b35c2f8eb65401cdbc36ae0eec76cf16f848bd2
StaceyO21 <staceyobrien21@gmail.com>
2024-05-30 19:52:53 -0500

    Part Three

--- more-on-functions/studio/part-two-create-sorted-array.js
+++ more-on-functions/studio/part-two-create-sorted-array.js
@@ -27,3 +27,17 @@ function findMinValue(arr){
 let nums1 = [5, 10, 2, 42];
 let nums2 = [-2, 0, -10, -44, 5, 3, 0, 3];
 let nums3 = [200, 5, 4, 10, 8, 5, -3.3, 4.4, 0];
+
+function sortNumberArray(array){
+  let sortedArray = [];
+  let length = array.length;
+  for (let index = 0; index < length; index++) {
+    let value = findMinValue(array);
+    sortedArray.push(value);
+    array.splice(array.indexOf(value), 1);
+  }
+  return sortedArray;
+}
+console.log(sortNumberArray(nums1));
+console.log(sortNumberArray(nums2));
+console.log(sortNumberArray(nums3));
