67b27ef272f82512be2428c6c0c65771aed05c4c
StaceyO21 <staceyobrien21@gmail.com>
2024-06-24 20:31:28 -0500

    test-student-labs

--- exceptions/exercises/divide.js
+++ exceptions/exercises/divide.js
@@ -1,7 +1,16 @@
 // Write a function called 'divide' that takes two parameters: a numerator and a denominator.
-
+const input = require("readline-sync");
 // Your function should return the result of numerator / denominator.
 
 // However, if the denominator is zero you should throw the error, "Attempted to divide by zero."
 
 // Code your divide function here:
+numerator = Number(input.question("Enter the numerator: "));
+denominator = Number(input.question("Enter the denominator: "))
+function divide (numerator, denominator){
+    if (denominator === 0) {
+        throw Error("Attempted to divide by zero.");
+    }
+   return numerator / denominator;
+}
+console.log(divide(numerator, denominator));
